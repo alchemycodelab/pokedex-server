34abe12f738e3ee13ffbbc11b174c76f34aa7900
martypdx <marty@martypdx.com>
2019-05-29 18:33:14 -0700

    paging and search on pokedex

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const http = require('http');
+const app = require('./lib/app');
+const connect = require('./lib/util/connect');
+
+const DB_URI = process.env.MONGODB_URI;
+connect(DB_URI);
+
+const server = http.createServer(app);
+const port = process.env.port || 3000;
+
+server.listen(port, () => {
+    // eslint-disable-next-line
+    console.log('server running on', server.address().port);
+});
